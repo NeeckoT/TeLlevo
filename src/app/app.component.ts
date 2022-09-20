c24ad0afe728f40ddc458bb2f2a7ca553dd6dcee
NeeckoT <62856691+NeeckoT@users.noreply.github.com>
2022-09-20 15:30:57 -0300

    Inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
+    { title: 'Iniciar Sesión', url: 'login', icon: 'people' },
+    { title: 'Inicio', url: 'home', icon: 'home' },
     { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
     { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
